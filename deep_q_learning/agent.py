1bbf0ab4c6f592c32e9bb77885298b75421d5206
L16H7 <lightcode2018@gmail.com>
2024-09-06 20:56:08 -0700

    update target model

--- deep_q_learning/agent.py
+++ deep_q_learning/agent.py
@@ -8,8 +8,11 @@ class Agent:
     def __init__(self, epsilon, input_dim, n_actions, gamma=0.99) -> None:
         self.epsilon = epsilon
         self.model = DQN(input_dim=input_dim, n_actions=n_actions)
+        self.target_model = DQN(input_dim=input_dim, n_actions=n_actions)
         self.gamma = gamma
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+        self.target_model.load_state_dict(self.model.state_dict())
 
     def choose_action(self, state):
         '''
@@ -27,13 +30,11 @@ class Agent:
 
         return random.choice([0, 1, 2, 3])
 
-    def learn(self, memory_buffer):
+    def learn(self, memory_buffer, replace):
         print('learn')
         states, actions, rewards, next_states, dones = memory_buffer
-        #self.memory.sample()
         # randomize the indices
         indices = np.random.permutation(128)
-        print(indices[0])
 
         # batch the data into 32 samples
         batch_size = 32
@@ -41,7 +42,6 @@ class Agent:
 
         for i in range(num_batches):
             batch_indices = indices[i*batch_size:(i+1)*batch_size]
-            print(batch_indices)
             batch_states = states[batch_indices]
             batch_actions = actions[batch_indices]
             batch_rewards = rewards[batch_indices]
@@ -53,7 +53,7 @@ class Agent:
             batch_dones = torch.tensor(batch_dones, dtype=torch.float32)
 
             # target = r + gamma * max(Q(s_t+1, a))
-            next_state_q_values = self.model.forward(batch_next_states) # Q_s_t+1
+            next_state_q_values = self.target_model.forward(batch_next_states) # Q_s_t+1
             max_q_values, _ = torch.max(next_state_q_values, axis=1)  # max(Q(s_t+1, a))
 
             batch_rewards = torch.tensor(batch_rewards, dtype=torch.float32)
@@ -70,3 +70,7 @@ class Agent:
             loss = torch.nn.MSELoss()(q_values, target)
             loss.backward()
             self.optimizer.step()
+
+            if replace:
+                self.target_model.load_state_dict(self.model.state_dict())
+                
--- deep_q_learning/dqn.py
+++ deep_q_learning/dqn.py
@@ -19,7 +19,7 @@ from memory import Memory
 from agent import Agent
 
 # env = gym.make("LunarLander-v3", render_mode="human")
-env = gym.make("LunarLander-v2")
+env = gym.make("LunarLander-v2", render_mode="human")
 
 memory = Memory()
 epsilon = 0.5
@@ -27,20 +27,26 @@ agent = Agent(epsilon=epsilon, input_dim=8, n_actions=4)
 
 n_episode = 100
 step = 0
-
-for _ in range(n_episode):
+for i in range(n_episode):
     state, _ = env.reset()
     done = False
 
+    total_reward = 0
     while not done:
         action = agent.choose_action(state)
         next_state, reward, done, _, _ = env.step(action)
         memory.store_memory(state, action, reward, next_state, done)
+        total_reward += reward
 
         state = next_state
         step += 1
         
         if step % 128 == 0:
             memory_buffer = memory.get_memory()
-            agent.learn(memory_buffer)
+
+            is_replace_target = step % 1024 == 0
+            agent.learn(memory_buffer, is_replace_target)
             memory.clear_memory()
+
+        if i % 10 == 0:
+            print(f"Episode {i}/{n_episode}, Total Reward: {total_reward}")
